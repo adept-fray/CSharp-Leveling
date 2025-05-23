1aab040f4f4948222117e288dada4872cbd13833
adept-fray <siavash.kh41@gmail.com>
2025-05-23 20:08:05 +0200

    Completed Simple Calculator

--- IO and File Handling/Simple Calculator/Program.cs
+++ IO and File Handling/Simple Calculator/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Simple Calculator");
+Console.WriteLine("Write two numbers and one operator");
+
+Console.WriteLine("First number: ");
+string fNum = Console.ReadLine();
+int firstNumber = int.Parse(fNum);
+
+Console.WriteLine("Second number: ");
+string sNum = Console.ReadLine();
+int secondNumber = int.Parse(sNum);
+
+Console.WriteLine("Operator: ");
+string op = Console.ReadLine();
+
+int result = 0;
+if (op == "+")
+    result = firstNumber + secondNumber;
+if (op == "-")
+    result = firstNumber - secondNumber;
+if (op == "*")
+    result = firstNumber * secondNumber;
+if (op == "/")
+    result = firstNumber / secondNumber;
+
+string output = $"Result is: {result}";
+Console.WriteLine(output);
+
+Console.ReadKey();
