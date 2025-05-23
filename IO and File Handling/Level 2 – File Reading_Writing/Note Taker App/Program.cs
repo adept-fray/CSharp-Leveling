a695c211c19462315b7a6753a762fef23eca637c
adept-fray <siavash.kh41@gmail.com>
2025-05-23 20:52:17 +0200

    Completed Note Taker App. Second time.

--- IO and File Handling/Level 2 – File Reading_Writing/Note Taker App/Program.cs
+++ IO and File Handling/Level 2 – File Reading_Writing/Note Taker App/Program.cs
@@ -1,11 +1,4 @@
-﻿Console.WriteLine("Note Taker App");
-Console.WriteLine("Save and read notes from a file.");
-
-Console.WriteLine("Write your note:");
-string note = Console.ReadLine();
-note = note + "\n";
-
-string fileName = "notes.txt";
+﻿string fileName = "notes.txt";
 
 string folderName = Environment.CurrentDirectory;
 folderName = Directory.GetParent(@"../../../").FullName;
@@ -13,12 +6,45 @@ folderName = folderName.Replace("\\", "/");
 
 string path = Path.Combine(folderName, fileName);
 
-if (File.Exists(path))
-    File.AppendAllText(path, note);
+
+Console.WriteLine("Note Taker App");
+Console.WriteLine("Save and read notes from a file.");
+
+
+Console.WriteLine("1. for writing note.");
+Console.WriteLine("2. for listing all notes.");
+string option = Console.ReadLine();
+if (option == "1")
+{
+    Console.WriteLine("Write your note:");
+    string note = Console.ReadLine();
+    note = note + "\n";
+
+    if (File.Exists(path))
+        File.AppendAllText(path, note);
+    else
+    {
+        Console.WriteLine("File does not exist. " + path);
+        File.WriteAllText(path, note);
+    }
+}
+else if (option == "2")
+{
+    if(File.Exists(path))
+    {
+        foreach(string note in File.ReadAllLines(path))
+        {
+            Console.WriteLine($"{note}");
+        }
+    }
+    else
+    {
+        Console.WriteLine("There are not notes.");
+    }
+}
 else
 {
-    Console.WriteLine("File does not exist. " + path);
-    File.WriteAllText(path, note);
+    Console.WriteLine("Uknown command / Incorrect option");
 }
 
 Console.ReadKey();
