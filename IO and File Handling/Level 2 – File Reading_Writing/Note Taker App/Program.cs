c9d2c1afc45c0b407f6217965eb91dbe2b4ff7dc
adept-fray <siavash.kh41@gmail.com>
2025-05-23 20:40:48 +0200

    Completed Note Taker App.

--- IO and File Handling/Level 2 – File Reading_Writing/Note Taker App/Program.cs
+++ IO and File Handling/Level 2 – File Reading_Writing/Note Taker App/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Note Taker App");
+Console.WriteLine("Save and read notes from a file.");
+
+Console.WriteLine("Write your note:");
+string note = Console.ReadLine();
+note = note + "\n";
+
+string fileName = "notes.txt";
+
+string folderName = Environment.CurrentDirectory;
+folderName = Directory.GetParent(@"../../../").FullName;
+folderName = folderName.Replace("\\", "/");
+
+string path = Path.Combine(folderName, fileName);
+
+if (File.Exists(path))
+    File.AppendAllText(path, note);
+else
+{
+    Console.WriteLine("File does not exist. " + path);
+    File.WriteAllText(path, note);
+}
+
+Console.ReadKey();
