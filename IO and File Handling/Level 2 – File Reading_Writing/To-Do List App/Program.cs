b26941448fb9874aaecd31a1baf0598422e45dc4
adept-fray <siavash.kh41@gmail.com>
2025-05-24 07:28:34 +0200

    created the menu options.

--- IO and File Handling/Level 2 – File Reading_Writing/To-Do List App/Program.cs
+++ IO and File Handling/Level 2 – File Reading_Writing/To-Do List App/Program.cs
@@ -11,4 +11,11 @@ if (folderName.Contains("To-Do List App"))
 folderName = folderName.Replace("\\", "/");
 string filePath = Path.Combine(folderName, fileName);
 
+void Menu()
+{
+    Console.WriteLine("(1) Add task");
+    Console.WriteLine("(2) Show tasks");
+    Console.WriteLine("(3) Delete task");
+}
+
 Console.ReadKey();
