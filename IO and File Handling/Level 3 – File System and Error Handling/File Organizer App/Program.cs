3875c090c0512f96dc84033a609873ed70b9837a
adept-fray <siavash.kh41@gmail.com>
2025-05-24 09:19:02 +0200

    checking for directory path.

--- IO and File Handling/Level 3 – File System and Error Handling/File Organizer App/Program.cs
+++ IO and File Handling/Level 3 – File System and Error Handling/File Organizer App/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("File Organizer App");
+
+Console.WriteLine("Write a directory path to orginize its contents.");
+string directoryPath = Console.ReadLine();
+
+if (!Directory.Exists(directoryPath))
+{
+    Console.WriteLine("Directory does not exists. exiting now.");
+    Console.WriteLine("Your directory path: " + directoryPath);
+    Console.ReadKey();
+    return;
+}
+
+Console.ReadKey();
