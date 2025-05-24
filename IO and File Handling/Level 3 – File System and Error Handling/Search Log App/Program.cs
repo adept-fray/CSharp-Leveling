847820aa973d044607b58b01b6e8d7137ba5b22b
adept-fray <siavash.kh41@gmail.com>
2025-05-24 15:50:59 +0200

    asking for filename and keyword. checking for file existance.

--- IO and File Handling/Level 3 – File System and Error Handling/Search Log App/Program.cs
+++ IO and File Handling/Level 3 – File System and Error Handling/Search Log App/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Search Log App");
+Console.WriteLine("Search for a keyword in a log file.");
+
+Console.WriteLine("Write absolute file name: ");
+string filepath = Console.ReadLine();
+
+Console.WriteLine("Write search keyword: ");
+string keyword = Console.ReadLine();
+
+if (!File.Exists(filepath))
+{
+    Console.WriteLine($"File does not exist.");
+    Console.WriteLine($"filename: {filepath}");
+    return;
+}
