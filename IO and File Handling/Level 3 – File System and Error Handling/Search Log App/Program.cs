3e4eb2e54676348e84995a85c4b9a6722b875a77
adept-fray <siavash.kh41@gmail.com>
2025-05-24 16:07:28 +0200

    Printing each line that contains keyword. only .txt files?

--- IO and File Handling/Level 3 – File System and Error Handling/Search Log App/Program.cs
+++ IO and File Handling/Level 3 – File System and Error Handling/Search Log App/Program.cs
@@ -11,5 +11,18 @@ if (!File.Exists(filepath))
 {
     Console.WriteLine($"File does not exist.");
     Console.WriteLine($"filename: {filepath}");
+    Console.ReadKey();
     return;
 }
+
+using (StreamReader sr = new StreamReader(filepath))
+{
+    string line;
+    while ((line = sr.ReadLine()) != null)
+    {
+        if (line.Contains(keyword))
+            Console.WriteLine(line);
+    }
+}
+
+Console.ReadKey();
