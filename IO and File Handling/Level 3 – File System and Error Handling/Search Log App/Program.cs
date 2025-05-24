55223f7e5bce71bb41995f05ee2647af06524e10
adept-fray <siavash.kh41@gmail.com>
2025-05-24 16:18:27 +0200

    removed if check and did it with exception in try catch. exception handling for file related errors.

--- IO and File Handling/Level 3 – File System and Error Handling/Search Log App/Program.cs
+++ IO and File Handling/Level 3 – File System and Error Handling/Search Log App/Program.cs
@@ -7,22 +7,42 @@ string filepath = Console.ReadLine();
 Console.WriteLine("Write search keyword: ");
 string keyword = Console.ReadLine();
 
-if (!File.Exists(filepath))
-{
-    Console.WriteLine($"File does not exist.");
-    Console.WriteLine($"filename: {filepath}");
-    Console.ReadKey();
-    return;
-}
 
-using (StreamReader sr = new StreamReader(filepath))
+try
 {
-    string line;
-    while ((line = sr.ReadLine()) != null)
+    using (StreamReader sr = new StreamReader(filepath))
     {
-        if (line.Contains(keyword))
-            Console.WriteLine(line);
+        string line;
+        while ((line = sr.ReadLine()) != null)
+        {
+            if (line.Contains(keyword))
+                Console.WriteLine(line);
+        }
     }
+}
+catch (FileNotFoundException ex)
+{
+    Console.WriteLine($"Error: The specified file was not found.");
+}
+catch (UnauthorizedAccessException ex)
+{
+    Console.WriteLine($"Error: You do not have permission to read this file.");
+}
+catch (DirectoryNotFoundException ex)
+{
+    Console.WriteLine($"Error: the directory for this file does not exist.");
+}
+catch (IOException ex)
+{
+    Console.WriteLine($"IO Error: {ex.Message}");
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine($"Error: The file path format is invalid.");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Unexpected error: {ex.Message}");
 }
 
 Console.ReadKey();
