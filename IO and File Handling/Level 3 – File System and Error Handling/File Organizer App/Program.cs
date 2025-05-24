0404c44154daf81c9f0a985c2d77339cb023a534
adept-fray <siavash.kh41@gmail.com>
2025-05-24 10:51:44 +0200

    cleaned from string manipulation to use Path Class.

--- IO and File Handling/Level 3 – File System and Error Handling/File Organizer App/Program.cs
+++ IO and File Handling/Level 3 – File System and Error Handling/File Organizer App/Program.cs
@@ -11,4 +11,45 @@ if (!Directory.Exists(directoryPath))
     return;
 }
 
+
+Dictionary<string, string> extensionAndFolderDict = new();
+extensionAndFolderDict.Add(".txt", "TextFiles");
+extensionAndFolderDict.Add(".jpg", "Images");
+extensionAndFolderDict.Add(".jpeg", "Images");
+extensionAndFolderDict.Add(".png", "Images");
+extensionAndFolderDict.Add(".cs", "CSharpFiles");
+
+List<string> files = new(Directory.GetFiles(directoryPath));
+foreach (string file in files)
+{
+    Console.WriteLine(file);
+    string extension = Path.GetExtension(file).ToLower();
+    Console.WriteLine(extension);
+
+    if (!extensions.Contains(extension))
+    {
+        continue;
+    }
+
+
+    string directoryName = extensionAndFolderDict.GetValueOrDefault(extension);
+    if (directoryName is null)
+        directoryName = "OtherFiles";
+
+    string currentDirectory = Path.GetDirectoryName(file);
+    string newDirectory = Path.Combine(currentDirectory, directoryName);
+    Console.WriteLine("newDirectory: " + newDirectory);
+    if (!Directory.Exists(newDirectory))
+    {
+        Directory.CreateDirectory(newDirectory);
+    }
+
+    string filename = Path.GetFileName(file);
+    Console.WriteLine("filename: " + filename);
+
+    string newFilePath = Path.Combine(newDirectory, filename);
+    Console.WriteLine("newFilePath: " + newFilePath);
+    File.Move(file, newFilePath);
+}
+
 Console.ReadKey();
--- IO and File Handling/Level 3 – File System and Error Handling/File Organizer App/README.md
+++ IO and File Handling/Level 3 – File System and Error Handling/File Organizer App/README.md
@@ -6,4 +6,4 @@ Organize files by extension.
 ### Steps:
 - Ask for a directory path.
 - Move <code>.txt</code>, <code>.jpg</code>, <code>.cs</code> files into subfolders like <code>TextFiles</code>, <code>/Images</code>, etc.
-- Use <code>Directory.GetFiles()</code> and <code>File.Movbe()</code>.
+- Use <code>Directory.GetFiles()</code> and <code>File.Move()</code>.
