7611fe322d2523e2c2c97c99b2f7605263ce984e
adept-fray <siavash.kh41@gmail.com>
2025-05-24 07:26:51 +0200

    create the absolute filepath to todo.txt file.

--- IO and File Handling/Level 2 – File Reading_Writing/To-Do List App/Program.cs
+++ IO and File Handling/Level 2 – File Reading_Writing/To-Do List App/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("To-Do List app");
+Console.WriteLine("a file-based task tracker.");
+
+string fileName = "todo.txt";
+string folderName = Environment.CurrentDirectory;
+if (folderName.Contains("To-Do List App"))
+{
+    folderName = Directory.GetParent(@"../../../").FullName;
+}
+
+folderName = folderName.Replace("\\", "/");
+string filePath = Path.Combine(folderName, fileName);
+
+Console.ReadKey();
