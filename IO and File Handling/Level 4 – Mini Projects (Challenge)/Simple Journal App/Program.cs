f49787c72368baa0958baad2cef80b7fd7b8dcdf
adept-fray <siavash.kh41@gmail.com>
2025-05-25 10:40:41 +0200

    Completed Simple Journal App. with no error handling.

--- IO and File Handling/Level 4 – Mini Projects (Challenge)/Simple Journal App/Program.cs
+++ IO and File Handling/Level 4 – Mini Projects (Challenge)/Simple Journal App/Program.cs
@@ -5,6 +5,24 @@ Console.WriteLine("Write daily entries.");
 string currentDirectory = Environment.CurrentDirectory;
 if(currentDirectory.Contains("Simple Journal App"))
 {
-    currentDirectory = Directory.GetParent(@"../../../");
+    currentDirectory = Directory.GetParent(@"../../../").FullName;
 }
 
+Console.WriteLine("Write Entry:");
+string entry = Console.ReadLine();
+if (string.IsNullOrWhiteSpace(entry))
+{
+    Console.WriteLine("can not save empty. Exiting.");
+    Console.ReadKey();
+    return;
+}
+var now = DateTime.UtcNow;
+DateOnly date = new DateOnly(now.Year, now.Month, now.Day);
+
+string filePath = Path.Join(currentDirectory,date.ToString() + ".txt");
+List<string> contents = new List<string> { entry };
+File.AppendAllLines(filePath, contents);
+
+
+Console.WriteLine("Exiting.");
+Console.ReadKey();
