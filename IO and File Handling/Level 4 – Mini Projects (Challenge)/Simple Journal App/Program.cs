7a8c5784f71271ed31af097c0ae43f28f05ffa8b
adept-fray <siavash.kh41@gmail.com>
2025-05-25 10:26:10 +0200

    Get current directory.

--- IO and File Handling/Level 4 – Mini Projects (Challenge)/Simple Journal App/Program.cs
+++ IO and File Handling/Level 4 – Mini Projects (Challenge)/Simple Journal App/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Simple Journal App");
+Console.WriteLine("Write daily entries.");
+
+
+string currentDirectory = Environment.CurrentDirectory;
+if(currentDirectory.Contains("Simple Journal App"))
+{
+    currentDirectory = Directory.GetParent(@"../../../");
+}
+
