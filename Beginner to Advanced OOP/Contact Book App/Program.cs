a43aa75436d290a1d455aebd8ac00f8b77c91eb6
adept-fray <siavash.kh41@gmail.com>
2025-05-26 16:04:03 +0200

    created Contact class and a list of Contacts.

--- Beginner to Advanced OOP/Contact Book App/Program.cs
+++ Beginner to Advanced OOP/Contact Book App/Program.cs
@@ -3,7 +3,33 @@ Console.WriteLine("Create a class Contact with Name, Phone, Email.");
 
 class Contact
 {
-    public string Name;
-    public string Phone;
-    public string Email;
+    public string Name { get; set; }
+    public string Phone { get; set; }
+    public string Email { get; set; }
+
+    public Contact(string name, string phone, string email)
+    {
+        Name = name;
+        Phone = phone;
+        Email = email;
+    }
+    public void Display()
+    {
+        Console.WriteLine($"Name: {Name}, Phone: {Phone}, Email: {Email}");
+    }
 }
+
+List<Contact> contacts = new List<Contact>()
+{
+    new Contact
+    {
+        Name = "name1",
+        Phone = "phone1",
+        Email = "email1"
+    },
+    new Contact {
+        Name = "name2",
+        Phone = "phone2",
+        Email = "email2"
+    }
+};
